4b21c548ac8484b8e04077fe131b536ecf7ff852
Akash Kumar <akash>
2025-09-29 22:59:30 +0530

    chatPage_modification

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -15,7 +15,7 @@ import {
 import { StreamChat } from "stream-chat";
 import toast from "react-hot-toast";
 
-import ChatLoader from "../components/chatLoader";
+import ChatLoader from "../components/ChatLoader";
 import CallButton from "../components/CallButton";
 
 const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
